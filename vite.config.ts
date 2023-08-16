1eeee0f30896032905c110fb2e6558adde77497f
Lev Kaplunov <limon4ik59rus@mail.ru>
2023-08-16 10:14:51 +0500

    add max-height for bottom-sheet

--- vite.config.ts
+++ vite.config.ts
@@ -7,25 +7,31 @@ import dns from 'dns';
 
 dns.setDefaultResultOrder('verbatim');
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    createSvgSpritePlugin({
-      include: '**/icons/**/*.svg',
-      symbolId: '[name]'
-    })
-  ],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "src/assets/style/_mixins.scss";`
+export default defineConfig((env) => {
+  const baseUrl = env.mode === 'production' ? '/4x8' : '/';
+
+  return {
+    plugins: [
+      vue(),
+      createSvgSpritePlugin({
+        include: '**/icons/**/*.svg',
+        symbolId: '[name]'
+      })
+    ],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "src/assets/style/_mixins.scss";`
+        }
       }
-    }
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '@ui': fileURLToPath(new URL('./src/components/ui', import.meta.url))
-    }
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+        '@ui': fileURLToPath(new URL('./src/components/ui', import.meta.url))
+      }
+    },
+
+    base: baseUrl,
   }
 });
